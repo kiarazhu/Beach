74d8ee491109fc2b48cfbe5e3cfd9facf5751111
tara564 <taramcmillanrice@gmail.com>
2024-08-17 16:42:12 +1000

    table?kind of?

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from operator import contains
+
 from flask import Flask, render_template, request
 import sqlite3
+import jinja2
 app = Flask(__name__)
 
 
@@ -20,6 +23,7 @@ def search():
     print(request.form['query'])
     searchTerm = request.form['query']
     result = queryBeach(searchTerm)
+
     html_table = "<table><tr><th>Location<\\th><th>Lifeguard Information<\\th><\\tr>"
     for row in result:
         html_table+="<tr><td>"
@@ -27,8 +31,23 @@ def search():
         html_table+="<td>" + row[1] + "<\\td><\\tr>"
     html_table+="<\\table>"
     print(html_table)
-    return html_table
-    return render_template('html_table', searchReturn = result, table = html_table)
+    html = ""
+    adding_table=False
+    with open("templates/index.html", mode="r") as htmlfile:
+        for line in htmlfile:
+            if not adding_table:
+                html += line
+            if line.find("<--start table -->")!=-1:
+                html+=html_table
+                adding_table=True
+            if line.find("<--end table-->")!=-1:
+                adding_table=False
+                html+=line
+    write_file = open("templates/index.html", "w")
+    write_file.write(html)
+    write_file.close()
+    #return html
+    return render_template('index.html', searchReturn = result, table = html_table)
 
 
 if __name__ == '__main__':
