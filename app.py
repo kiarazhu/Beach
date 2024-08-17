318b2f364d609db69654dc5f648235d3a9bdf92b
tara564 <taramcmillanrice@gmail.com>
2024-08-17 16:05:32 +1000

    -

--- app.py
+++ app.py
@@ -13,17 +13,25 @@ def queryBeach(searchTerm):
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('search.html', message = "")
+    return render_template('index.html', message = "")
 
 @app.route('/search', methods = ['POST'])
 def search():
     print(request.form['query'])
     searchTerm = request.form['query']
     result = queryBeach(searchTerm)
-    return render_template('search.html', message = result)
+    html_table = "<table><tr><th>Location<\\th><th>Lifeguard Information<\\th><\\tr>"
+    for row in result:
+        html_table+="<tr><td>"
+        html_table+=row[2] + "<\\td>"
+        html_table+="<td>" + row[6] + "<\\td><\\tr>"
+    html_table+="<\\table>"
+    print(html_table)
+    return html_table
+    return render_template('html_table', searchReturn = result, table = html_table)
 
 
 if __name__ == '__main__':
     app.run()
 
-#POO
+
