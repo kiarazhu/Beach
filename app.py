2f7cfcbeb821c81809958f1b5b7127fe611176b8
tara564 <taramcmillanrice@gmail.com>
2024-08-18 09:25:05 +1000

    lang

--- app.py
+++ app.py
@@ -6,7 +6,33 @@ import sqlite3
 import jinja2
 app = Flask(__name__)
 
+text = {"Chinese": ["为了安全游泳，请在旗帜之间游泳，并与朋友一起。不在超过您舒适范围的深水或波涛汹涌的水域游泳。如果不确定，可以向救生员询问有关海况的信息！",
+                    "澳大利亚的海滩有标志帮助您了解是否适合游泳：",
+                    "如果您在水中遇到困难可以举起手臂并从一侧挥到另一侧向救生员示意求助。",
+                    "澳大利亚的海滩可能有不同种类的野生动物，其中一些可能是危险的。如果它是活的，请保持距离"],
+        "English": ["To swim safely, swim between the flags, with a friend. Don’t swim in water that is deeper or rougher than you are comfortable. You can ask the lifeguards for information about the surf conditions if you are unsure!",
+                    "Beaches in Australia have signs to help you understand whether it is safe to swim or not:",
+                    "If you are in trouble in the water then you can signal for help from a lifeguard by raising your arm and waving it from side to side.",
+                    "Australian beaches can contain different wildlife species, some of which can be dangerous. If it is alive, give it space."]}
+flags = {"English": {"RedYellow": "Red & Yellow Flags: Swim between the flags. This area will be safe to swim in.",
+                     "Red": "Red Flag: No Swimming.",
+                     "Yellow": "Yellow Flag: Caution required. Potential hazards.",
+                     "RedWhite": "Red & White Flag: Evacuate the water.",
+                     "BlackWhite": "Black & White Flag: Surfcraft riding area boundary"},
+         "Chinese": {"RedYellow": "红黄旗：在旗帜之间游泳。这一区域适合游泳。",
+                     "Red": "红旗：禁止游泳。",
+                     "Yellow": "黄旗：需要谨慎。可能存在潜在的危险。",
+                     "RedWhite": "红白旗：立即撤离水域。",
+                     "BlackWhite": "黑白旗：冲浪板区域边界。"}}
 
+warnings = {"English": {"Warning": "Warning",
+                        "NoSwim": "Swimming not advised",
+                        "Waves": "Large waves",
+                        "Stingers": "Marine stingers"},
+            "Chinese": {"Warning": "Warning",
+                        "NoSwim": "Swimming not advised",
+                        "Waves": "Large waves",
+                        "Stingers": "Marine stingers"}}
 def queryBeach(searchTerm):
     query = f"SELECT DISTINCT location, life_guard_service  FROM Beach WHERE location LIKE '%{searchTerm}%';"
     connection = sqlite3.connect("beach.db")
@@ -17,7 +43,21 @@ def queryBeach(searchTerm):
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html', message = "")
+    return render_template('index.html',
+                           text=text["English"],
+                           flags=flags["English"],
+                           warnings=warnings["English"])
+
+
+@app.route('/switchLang', methods=['POST'])
+def switch():
+    print(request.form['lang'])
+
+    return render_template('index.html',
+                           text = text[request.form['lang']],
+                           flags=flags[request.form['lang']],
+                           warnings=warnings[request.form['lang']])
+
 
 @app.route('/search', methods = ['POST'])
 def search():
